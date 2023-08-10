a2cbbfe1d5b6a7fe643c59fa7a2f258719de27fa
KartikayR-SI <140160409+KartikayR-SI@users.noreply.github.com>
2023-08-10 11:48:48 +0530

    Add files via upload
    Control Structures problems

---
+++ CheckCredit.cs
@@ -0,0 +1,22 @@
+﻿namespace ControlStructures
+{
+    internal class CheckCredit
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Enter the price for an phone");
+            int pur_price = Convert.ToInt32(Console.ReadLine());
+
+            int credit_limit = 10000;
+
+            if (pur_price > credit_limit)
+            {
+                Console.WriteLine("Value entered is more than the credit limit");
+            }
+            else
+            {
+                Console.WriteLine("Approved");
+            }
+        }
+    }
+}
---
+++ TemperatureTracker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStructures
+{
+    internal class TemperatureTracker
+    {
+        public static void Main(String[] Args)
+        {
+            int sentinel_val = 131;
+
+            double n;
+            double total = 0;
+
+            List<double> temp_list = new List<double>();
+            
+            do
+            {
+                Console.WriteLine("Enter the temperatures between 20 and 130 (131 to exit): ");
+                n = Convert.ToDouble(Console.ReadLine());
+                temp_list.Add(n);
+                total += n;
+            }while (n != sentinel_val);
+
+            int denominator = temp_list.Count;
+
+            Console.WriteLine("The temperature loop has ended");
+            Console.WriteLine("The temperatures entered by the user:");
+            foreach (var m in temp_list)
+            {
+                Console.WriteLine(m);
+            }
+            Console.WriteLine("\n The average temperature: "+(total/denominator));
+            
+
+        }
+
+    }
+}
